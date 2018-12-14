52ad9d343dbe7e8eba5384d3e9cc23c07c138789
lyj <574473819@qq.com>
2018-12-14 22:52:09 +0800

    Update me.js
    完善所有页面，加入关注的状态变化，对于问题和答案写了方法控制字数显示

--- community-user/community/pages/me/me.js
+++ community-user/community/pages/me/me.js
@@ -1,73 +1,95 @@
 // pages/me/me.js
+var util = require('../../util/util.js')
+var app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        navTab: ["提问", "回答"],
-        currentNavtab: "0",
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navTab: ["提问", "回答"],
+    currentNavtab: "0",
+    questions:[],
+    focus:[],
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this;
+    this.getData();
+    this.getData3();
+  },
 
-    }, 
-    
-    switchTab: function (e) {
-        this.setData({
-            currentNavtab: e.currentTarget.dataset.idx
-        });
-    },
+  switchTab: function(e) {
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  getData: function() {
+    var questions = util.getData2();
+    var questions_data = questions.data;
+    this.setData({
+      questions:questions_data
+    });
+  },
 
-    },
+  getData3: function () {
+    var focus = util.getData3();
+    var focus = focus.data;
+    this.setData({
+      focus: focus,
+      focus_data_length: focus.length
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+  },
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+  },
 
-    }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
 })
