4300b2a223607ce65a44a98b4dbf1730bc1a3e88
lyj <574473819@qq.com>
2018-12-14 22:51:11 +0800

    Update focus_person.js
    完善所有页面，加入关注的状态变化，对于问题和答案写了方法控制字数显示

--- community-user/community/data/focus_person.js
+++ community-user/community/data/focus_person.js
@@ -12,7 +12,11 @@ var focus = {
             question_id: 1,
             question_content: "Kindle和纸质书哪个更好",
             answer_id:1,
-            answer_content:"难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了..."
+            answer_content:"难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            answer_time:"2018-12-14 12:02",
+            ranker_id:2,
+            ranker:"Alex",
+            comment_time: "2018-12-14 12:12",
           },
           {
             user_id: 2,
@@ -21,7 +25,11 @@ var focus = {
             question_id: 1,
             question_content: "Kindle和纸质书哪个更好",
             answer_id: 1,
-            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了..."
+            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            answer_time: "2018-12-14 11:02",
+            ranker_id: 5,
+            ranker: "Lily",
+            comment_time: "2018-12-14 12:32",
           },
 
           {
@@ -31,7 +39,11 @@ var focus = {
             question_id:1,
             question_content: "Kindle和纸质书哪个更好",
             answer_id: 1,
-            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了..."
+            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            answer_time: "2018-12-14 11:00",
+            ranker_id: 2,
+            ranker: "Alex",
+            comment_time: "2018-12-14 12:42",
           },
           
           {
@@ -41,7 +53,11 @@ var focus = {
             question_id: 1,
             question_content: "Kindle和纸质书哪个更好",
             answer_id: 1,
-            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了..."
+            answer_content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            answer_time: "2018-12-14 10:02",
+            ranker_id: 2,
+            ranker: "Alex",
+            comment_time: "2018-12-14 12:52",
           }
         ]
 }
