374479b4c90435fcba23ec59e4de785fc762ecd1
lyj <574473819@qq.com>
2018-12-14 22:52:40 +0800

    Update notify.js
    完善所有页面，加入关注的状态变化，对于问题和答案写了方法控制字数显示

--- community-user/community/pages/notify/notify.js
+++ community-user/community/pages/notify/notify.js
@@ -1,66 +1,80 @@
 // pages/notify/notify.js
+var util = require('../../util/util.js')
+var app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 页面的初始数据
+   */
+  
+  data: {
+    questions: [],
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.getData();
+  },
+  //使用本地 fake 数据实现刷新效果
+  getData: function () {
+    var questions = util.getData3();
+    console.log("loaddata");
+    var questions_data = questions.data;
+    this.setData({
+      questions: questions_data,
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  }
 })
